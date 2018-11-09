7bc2dbc22041bfe343820d32b1a358726b2c508f
Camilla Björn <cabjorn@kth.se>
2018-11-09 01:18:18 +0100

    runge-kutta is working

--- diffeq.m
+++ diffeq.m
@@ -1,9 +1,11 @@
 tspan = [0 0.01];
 I_01 = [0; 240];
-N = 10;
-[t,I] = RK4(@current_ode, tspan, N, I_01);
-% options = odeset('RelTol',1e-10);
-% [t, I] = ode45(@current_ode, tspan, I_01, options);
+N = 100;
+[trk,Irk] = RK4(@current_ode, tspan, N, I_01);
+options = odeset('RelTol',1e-10);
+[t, I] = ode45(@current_ode, tspan, I_01, options);
 
-%plot(t, I(:,1),'-o', t, I(:,2), '-o')
+figure(1)
 plot(t, I(:,1),'-o', t, I(:,2), '-o')
+figure(2)
+plot(trk, Irk(1,:),'-o')
