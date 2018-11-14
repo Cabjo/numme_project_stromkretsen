0a582b88505a8334c2ced31941dc29ea19174d9b
Camilla Björn <cabjorn@kth.se>
2018-11-14 14:29:06 +0100

    ak finaly works :)

--- integral.m
+++ integral.m
@@ -1,15 +1,16 @@
 function S = integral(f, t, n, k)
-    max_idx = length(t);
-    h_idx = floor(max_idx/n)
-    T = t(max_idx);
-    w = 2*pi/T;
+    max_idx = length(t); %Maximum index is the length of t which is the "Last value"
+    h_idx = round(max_idx/n); %Take the maximum index av divide it by number of iterations
+    T = t(max_idx); % Periodtime is the value of t by the maximum index
+    w = 2*pi/T; %W is dependent of T which is the periodtime
     f_n = zeros();      % Contains function I evaluated in h*i
     t_n = zeros();      % Contains the times that I is evaluated in
     idx_time = 1;       % i
+    
     f_n(idx_time) = f(idx_time);
     t_n(idx_time) = t(idx_time);
     i = 2;
-    while idx_time+h_idx < max_idx 
+    while idx_time+h_idx < max_idx % Continues till the current value(index) plus steplength(index) is bigger the maximum value(index) 
         idx_time = idx_time+h_idx;
         t_n(i) = t(idx_time);
         f_n(i) = f(idx_time);
@@ -18,10 +19,10 @@ function S = integral(f, t, n, k)
     idx_time = max_idx;
     t_n(i) = t(idx_time);
     f_n(i) = f(idx_time);
-    
-    
+     
     % S is the integral of I(t)*sin(k*w*t) from 0 to T
     argument= k*w*t_n;
     sinus = sin(argument);
     S = t(h_idx)/2*(f_n(1)*sinus(1)+f_n(end)*sinus(end)+2*sum(f_n(2:end-1).*sinus(2:end-1)));  
+%     S = t(h_idx)/2*(f_n(1)+f_n(end)+2*sum(f_n(2:end-1))); 
 end
